7d95436dfb0ff23aacd1990a837022189743d5fe
ECWireless <Elliott@valtechcreative.com>
2021-01-20 13:00:15 -0700

    Add transfer test

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,65 @@ import { JWKInterface } from 'arweave/node/lib/wallet'
 import useArweave from './hooks/useArweave'
 import useContract from './hooks/useContract'
 
+// Consts
+const JOSEPH_WALLET_ADDRESS = 'O6SGGaUbSm72rQO-9A7SGFUIGOiSy8Uih1-zbQmufaU'
+const AMOUNT_TO_SEND = 100
+
 function App() {
-	const [wallet, setWallet] = React.useState<JWKInterface | null>(null)
+  const [loginError, setLoginError] = React.useState<boolean>(false)
+  const [stateRead, setStateRead] = React.useState<boolean>(false)
+  const [transferTxId, setTransferTxId] = React.useState<string | false>('')
+  const [wallet, setWallet] = React.useState<JWKInterface | null>(null)
 
   // Hooks
-  const arweave = useArweave()
-  const { getContractState } = useContract()
+  const { getContractState, onTransfer } = useContract(wallet! as JWKInterface)
+
+  // Upload wallet
+	const uploadWallet = (evt: React.ChangeEvent<HTMLInputElement>) => {
+		const fileReader = new FileReader()
+		
+		fileReader.onload = async (e) => {
+			try {
+				setWallet(JSON.parse(e.target!.result as string))
+			} catch (err) {
+				setLoginError(true)
+				console.error('Invalid wallet was uploaded.', err)
+			}
+		}
+		
+		if (evt.target.files?.length) {
+			fileReader.readAsText(evt.target.files[0])
+		}
+	}
 
   const onGetContractState = async () => {
     const contractState = await getContractState()
+    setStateRead(true)
     console.log(contractState)
+  }
+
+  const onTransferToken = async () => {
+    const trasactionId = await onTransfer(JOSEPH_WALLET_ADDRESS, AMOUNT_TO_SEND)
+    setTransferTxId(trasactionId)
+    console.log(trasactionId)
   }
 
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>Hooks Testing</h1>
-      <div style={{ width: '50%', display: 'flex', justifyContent: 'space-between', margin: '0 auto' }}>
+      {!wallet ? (
+      <div style={{ width: '50%', margin: '0 auto' }}>
+        <p>Upload Wallet</p>
+        <input type="file" onChange={uploadWallet} />
+      </div>)
+      : (<div style={{ width: '50%', margin: '0 auto' }}>
         <button onClick={onGetContractState}>Get Contract State</button>
-      </div>
+        <br />
+        {stateRead && <p>State was logged to the console.</p>}
+        <br />
+        <button onClick={onTransferToken}>Transfer</button>
+        {transferTxId && <p>{transferTxId}</p>}
+      </div>)}
     </div>
   );
 }
--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -1,26 +1,22 @@
-import { readContract } from 'smartweave'
-// import { JWKInterface } from 'arweave/node/lib/wallet'
+import { interactWrite, readContract } from 'smartweave'
+import { JWKInterface } from 'arweave/node/lib/wallet'
 // import { useState } from 'react'
 // import { ContractInterface, ContractList } from './interfaces'
 import useArweave from './useArweave'
 
 const CONTRACT_ADDRESS = 'qoUAMfYEBVOLQoQgqvbTyv302UVaFfl58jZow46sHhc'
 
-export default function useContracts() {
+export default function useContracts(wallet: JWKInterface) {
     const arweave = useArweave()
 
-    // const [contracts, setContracts] = useState<ContractList>({})
-
-    // const addMarket = async (contract: ContractInterface): Promise<string | false> => {
-    //     const contractId = await interactWrite(arweave, wallet, CONTRACT_ADDRESS, {
-    //         function: 'create',
-    //         contract
-    //     })
-    //     const newContracts: ContractList = contracts
-    //     newContracts[contractId as string] = contract
-    //     setContracts(newContracts)
-    //     return contractId
-    // }
+    const onTransfer = async (target: string, qty: number): Promise<string | false> => {
+        const txId = await interactWrite(arweave, wallet, CONTRACT_ADDRESS, {
+            function: 'transfer',
+            target,
+            qty,
+        })
+        return txId
+    }
 
     const getContractState = async (): Promise<any> => {
         const contractState = await readContract(arweave, CONTRACT_ADDRESS)
@@ -28,5 +24,5 @@ export default function useContracts() {
     }
 
 
-    return { getContractState }
+    return { getContractState, onTransfer }
 }
